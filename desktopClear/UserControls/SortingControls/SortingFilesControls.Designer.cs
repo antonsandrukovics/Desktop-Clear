5f3a7e18e60758231493ab9320bb086642612d92
Antons Andrukovics <antonsandrukovics@gmail.com>
2021-07-23 14:07:58 +0300

    Переименование визуальных компонентов, переменных, аргументов

--- desktopClear/UserControls/SortingControls/SortingFilesControls.Designer.cs
+++ desktopClear/UserControls/SortingControls/SortingFilesControls.Designer.cs
@@ -30,15 +30,15 @@ namespace desktopClear.UserControls.SortingControls
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.sortinglistView = new System.Windows.Forms.ListView();
             this.folderName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.extension = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-            this.button3 = new System.Windows.Forms.Button();
+            this.extensionsFilesCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.addBtn = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.sortBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -52,19 +52,19 @@ namespace desktopClear.UserControls.SortingControls
             this.label1.TabIndex = 0;
             this.label1.Text = "Форматы файлов";
             // 
-            // listView1
+            // sortinglistView
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.sortinglistView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.folderName,
             this.extension});
-            this.listView1.HideSelection = false;
-            this.listView1.LabelEdit = true;
-            this.listView1.Location = new System.Drawing.Point(261, 29);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(255, 351);
-            this.listView1.TabIndex = 2;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.sortinglistView.HideSelection = false;
+            this.sortinglistView.LabelEdit = true;
+            this.sortinglistView.Location = new System.Drawing.Point(261, 29);
+            this.sortinglistView.Name = "sortinglistView";
+            this.sortinglistView.Size = new System.Drawing.Size(255, 351);
+            this.sortinglistView.TabIndex = 2;
+            this.sortinglistView.UseCompatibleStateImageBehavior = false;
+            this.sortinglistView.View = System.Windows.Forms.View.Details;
             // 
             // folderName
             // 
@@ -76,25 +76,25 @@ namespace desktopClear.UserControls.SortingControls
             this.extension.Text = "Формат файлов";
             this.extension.Width = 100;
             // 
-            // checkedListBox1
+            // extensionsFilesCheckedListBox
             // 
-            this.checkedListBox1.CheckOnClick = true;
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(11, 31);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(110, 349);
-            this.checkedListBox1.TabIndex = 5;
+            this.extensionsFilesCheckedListBox.CheckOnClick = true;
+            this.extensionsFilesCheckedListBox.FormattingEnabled = true;
+            this.extensionsFilesCheckedListBox.Location = new System.Drawing.Point(11, 31);
+            this.extensionsFilesCheckedListBox.Name = "extensionsFilesCheckedListBox";
+            this.extensionsFilesCheckedListBox.Size = new System.Drawing.Size(110, 349);
+            this.extensionsFilesCheckedListBox.TabIndex = 5;
             // 
-            // button3
+            // addBtn
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(133, 191);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(116, 30);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Добавить";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.addBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addBtn.Location = new System.Drawing.Point(133, 191);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(116, 30);
+            this.addBtn.TabIndex = 7;
+            this.addBtn.Text = "Добавить";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.button3_Click);
             // 
             // label2
             // 
@@ -118,40 +118,40 @@ namespace desktopClear.UserControls.SortingControls
             this.label3.TabIndex = 9;
             this.label3.Text = "Сортировочные папки";
             // 
-            // button1
+            // saveBtn
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(133, 270);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 30);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Сохранить";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.saveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveBtn.Location = new System.Drawing.Point(133, 270);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(116, 30);
+            this.saveBtn.TabIndex = 10;
+            this.saveBtn.Text = "Сохранить";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // sortBtn
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(130, 349);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(122, 30);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Сортировать";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.sortBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sortBtn.Location = new System.Drawing.Point(130, 349);
+            this.sortBtn.Name = "sortBtn";
+            this.sortBtn.Size = new System.Drawing.Size(122, 30);
+            this.sortBtn.TabIndex = 11;
+            this.sortBtn.Text = "Сортировать";
+            this.sortBtn.UseVisualStyleBackColor = true;
+            this.sortBtn.Click += new System.EventHandler(this.button2_Click);
             // 
             // SortingFilesControls
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sortBtn);
+            this.Controls.Add(this.saveBtn);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.checkedListBox1);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.extensionsFilesCheckedListBox);
+            this.Controls.Add(this.sortinglistView);
             this.Controls.Add(this.label1);
             this.Name = "SortingFilesControls";
             this.Size = new System.Drawing.Size(527, 399);
@@ -163,14 +163,14 @@ namespace desktopClear.UserControls.SortingControls
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView sortinglistView;
         private System.Windows.Forms.ColumnHeader folderName;
         private System.Windows.Forms.ColumnHeader extension;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckedListBox extensionsFilesCheckedListBox;
+        private System.Windows.Forms.Button addBtn;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button sortBtn;
     }
 }
